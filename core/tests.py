7c7a435047ee70e1f382dd3f285e43789e89eac6
Danford Nchembi <danfordnchembi@gmail.com>
2021-01-07 16:10:05 +0300

    folder changes

--- core/tests.py
+++ core/tests.py
@@ -17,10 +17,10 @@ class TestFileHandling(unittest.TestCase):
         # self.out_dir = '/Users/user/Documents/EMR/out'
         # self.err_dir = '/Users/user/Documents/EMR/err'
 
-        self.root_dir = '/home/danny/EMR/'
-        self.in_dir = '/home/danny/EMR/in'
-        self.out_dir = '/home/danny/EMR/out'
-        self.err_dir = '/home/danny/EMR/err'
+        self.root_dir = '/EMR/'
+        self.in_dir = '/EMR/in'
+        self.out_dir = '/EMR/out'
+        self.err_dir = '/EMR/err'
 
         csv_file_handling = CsvFileHandling()
         for filename in os.listdir(self.root_dir):
@@ -33,10 +33,10 @@ class TestFileHandling(unittest.TestCase):
         self.send_daily_death_count_url = 'http://139.162.143.150:5001/daily_death_count'
         self.send_revenue_url = 'http://139.162.143.150:5001/revenue_received'
 
-        self.root_dir = '/home/danny/EMR/'
-        self.in_dir = '/home/danny/EMR/in'
-        self.out_dir = '/home/danny/EMR/out'
-        self.err_dir = '/home/danny/EMR/err'
+        self.root_dir = '/EMR/'
+        self.in_dir = '/EMR/in'
+        self.out_dir = '/EMR/out'
+        self.err_dir = '/EMR/err'
 
         csv_file_handling = CsvFileHandling()
 
