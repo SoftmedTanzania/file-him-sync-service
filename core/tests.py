bca28f76442d9b4806fbf5962d4a08620bc8c066
Danford Nchembi <danfordnchembi@gmail.com>
2021-01-09 01:34:41 +0300

    creating a config tool for file sync

--- core/tests.py
+++ core/tests.py
@@ -1,16 +1,22 @@
 from .tasks import prepare_csv_file_queue, post_csv_files
 import unittest
-from .models import Mediator, File, FilePath
+from .models import Mediator, FilePath
 
 # Create your tests here.
 class TestFileHandling(unittest.TestCase):
     FilePath.objects.all().delete()
 
+    # mediator = Mediator.objects.get(id=1)
+    # FilePath.objects.create(mediator=mediator, file_path='/Users/user/Documents/EMR/err', path_type='err_dir')
+    # FilePath.objects.create(mediator=mediator, file_path='/Users/user/Documents/EMR/in', path_type='in_dir')
+    # FilePath.objects.create(mediator=mediator, file_path='/Users/user/Documents/EMR/out', path_type='out_dir')
+    # FilePath.objects.create(mediator=mediator, file_path='/Users/user/Documents/EMR/', path_type='root_dir')
+
     mediator = Mediator.objects.get(id=1)
-    FilePath.objects.create(mediator=mediator, file_path='/Users/user/Documents/EMR/err', path_type='err_dir')
-    FilePath.objects.create(mediator=mediator, file_path='/Users/user/Documents/EMR/in', path_type='in_dir')
-    FilePath.objects.create(mediator=mediator, file_path='/Users/user/Documents/EMR/out', path_type='out_dir')
-    FilePath.objects.create(mediator=mediator, file_path='/Users/user/Documents/EMR/', path_type='root_dir')
+    FilePath.objects.create(mediator=mediator, file_path='/home/danny/EMR/err', path_type='err_dir')
+    FilePath.objects.create(mediator=mediator, file_path='/home/danny/EMR/in', path_type='in_dir')
+    FilePath.objects.create(mediator=mediator, file_path='/home/danny/EMR/out', path_type='out_dir')
+    FilePath.objects.create(mediator=mediator, file_path='/home/danny/EMR/', path_type='root_dir')
 
     def test_file_handling(self):
 
